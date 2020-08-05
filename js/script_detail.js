94975dae4a93bc2d8688fe25c153ed2788895116
Duy Uyen <uyenphanbinh@gmail.com>
2020-08-05 17:32:14 +0700

    update detail telephone

--- js/script_detail.js
+++ js/script_detail.js
@@ -1,1 +1,11 @@
-
+$(function (){
+    var star = '.star',
+        selected = '.selected';
+    
+    $(star).on('click', function(){
+      $(selected).each(function(){
+        $(this).removeClass('selected');
+      });
+      $(this).addClass('selected');
+    }); 
+});
