78b3a4de882df6c9ca6b38088b5b28bb40e7df67
18126025 <minhnn1403@gmail.com>
2020-09-23 21:55:01 +0700

    db_bill

--- a.js
+++
@@ -1,4 +0,0 @@
-var today = new Date();
- var time = 'Date: ' + today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear() +'  Time:'+ today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
- 
-console.log(time)
