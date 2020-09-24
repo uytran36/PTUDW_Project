e40d5d92bbb3f0683655af6243af3e1a7ab276b9
18126025 <minhnn1403@gmail.com>
2020-09-24 11:15:17 +0700

    up

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,5 @@
 
-// var bill = require('../models/bill.model');
+var bill = require('../models/bill.model');
 const { render } = require('pug');
 
 const Home = (req, res) => {
@@ -20,7 +20,16 @@ const loggedin = (req,res) => {
 };
 const login = (req,res) => {
     if(req.session.isLoggedIn)
-        res.render('admin');
+    {
+        bill.find().then(function(bill){
+            if(bill.length == 0)
+                throw new Error('Not found');
+            res.render('admin', {bill: bill})
+        })
+        .catch(() => {
+            res.render('notfound')
+        })
+    }
     else
         res.redirect('/admin');
 };
@@ -28,31 +37,23 @@ const logout = (req,res) => {
     req.session.isLoggedIn = false;
 	res.redirect('/admin');
 }
-    // bill.find().then(function(bill){
-    //     if(bill.length == 0)
-    //         throw new Error('Not found');
-    //     res.render('admin', {bill: bill})
-    // })
-    // .catch(() => {
-    //     res.render('notfound')
-    // })
 
-// const bill_detail = (req, res) => {
-//     var id = req.params.id;
-//     bill.find({_id:id}).then(function(bills){
-//         if(bills.length == 0)
-//             throw new Error('Not found');
-//         res.render('admin_detail_bill', {bill: bills[0]})
-//     })
-//     .catch(() => {
-//         res.render('notfound')
-//     })
-// }
+const bill_detail = (req, res) => {
+    var id = req.params.id;
+    bill.find({_id:id}).then(function(bills){
+        if(bills.length == 0)
+            throw new Error('Not found');
+        res.render('admin_detail_bill', {bill: bills[0]})
+    })
+    .catch(() => {
+        res.render('notfound')
+    })
+}
 
 module.exports = {
     Home,
     login,
     loggedin,
     logout,
-    //bill_detail
+    bill_detail
 }
